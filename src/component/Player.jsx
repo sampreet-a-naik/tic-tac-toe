23cd0773ea5a344339e92a4f863dfbd9c9d06c1f
sampreet-a-naik <sampreet.github@gmail.com>
2024-03-24 19:38:49 -0500

    added players comp

---
+++ src/component/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function editHandler() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function nameChangeHandler(event) {
+    setPlayerName(event.target.value);
+  }
+  return (
+    <li>
+      <span>
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={nameChangeHandler}
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editHandler}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
